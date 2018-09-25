fb40516bc1de8afb83159ea3432de9ec9e620832
Przemysław Lenart <przemek.lenart@polidea.com>
2018-09-25 10:27:04 +0200

    Upgrade to React Native 0.57.1

--- ios/File.swift
+++ ios/File.swift
@@ -2,7 +2,7 @@
 //  File.swift
 //  SensorTag
 //
-//  Created by Przemysław Lenart on 20/07/2018.
+//  Created by Przemysław Lenart on 24/09/2018.
 //  Copyright © 2018 Facebook. All rights reserved.
 //
 
